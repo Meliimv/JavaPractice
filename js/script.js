e99bc0e0e722d09ae27f93c8fbc888e76cbabb27
Melissa <mvmelissas@gmail.com>
2022-07-10 18:20:38 -0500

    entrega

--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
-let claveSecreta = "Hay una serpiente en mi bota";
-let mamáAdivina = prompt("cual crees que es la clave secreta?");
+/*Bienvenida al sitio*/
 
-while(mamáAdivina!=claveSecreta){
-    alert("No puedes entrar");
-    mamáAdivina = prompt("cual crees que es la clave secreta?");
+let nombreUsuario = prompt("Ingresa tu nombre")
+alert("Bienvenid@,  " + nombreUsuario)
+
+
+/*digite su clave*/
+
+let claveGuardada = "Colombia2022";
+let ingresar = false;
+for (let i = 2; i >= 0; i--) {
+    let claveUsuario = prompt("Ingresa tu clave");
+    if (claveGuardada == claveUsuario) {
+        alert("Ingresaste exitosamente");
+        ingresar = true;
+        break;
+    } else {
+        alert("error, te quedan " + i + " intentos")
+    }
 
 }
 
-alert(claveSecreta + "  eres bienvenida!")
+/*opciones carrito*/
+
+if (ingresar) {
+    let AlimentoParaMascota = "felino";
+    let opcion = prompt("Escoge el alimento que desees: \n1-Monello 7kg $90.000 sin IVA. \n Presiona x para salir.");
+    while (opcion != "x" && opcion != "X") {
+        switch (opcion) {
+            case "1":
+                let opcion = prompt("escoge una opcion: \n1-Ir a pagar. \n salir");
+                while (opcion != "salir" && opcion != "SALIR") {
+                    switch (opcion) {
+                        case "1":
+                            const suma = (a, b) => a + b;
+                            const resta = (a, b) => a - b;
+                            const iva = x => x * 0.19
+                            let precioProducto = 90.000
+                            let descuento = 15.000
+                            let precioTotal = resta(suma(precioProducto, iva(precioProducto)), descuento)
+                            console.log(precioTotal);
+
+                            let total = ("Son $92.900");
+                    }
+
+
+                }
+
+        }
+
+    }
+
+}
