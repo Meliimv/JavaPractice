9d5306128e0745f1bfdc37d1bc8e3926e0f2c6b1
Melissa <mvmelissas@gmail.com>
2022-07-11 20:47:25 -0500

    arrays complementario

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 /*Bienvenida al sitio*/
 
-let nombreUsuario = prompt("Ingresa tu nombre")
+/*let nombreUsuario = prompt("Ingresa tu nombre")
 alert("Bienvenid@,  " + nombreUsuario)
 
 
 /*digite su clave*/
 
-let claveGuardada = "Colombia2022";
+/*let claveGuardada = "Colombia2022";
 let ingresar = false;
 for (let i = 2; i >= 0; i--) {
     let claveUsuario = prompt("Ingresa tu clave");
@@ -22,7 +22,7 @@ for (let i = 2; i >= 0; i--) {
 
 /*opciones carrito*/
 
-if (ingresar) {
+/*if (ingresar) {
     let AlimentoParaMascota = "felino";
     let opcion = prompt("Escoge el alimento que desees: \n1-Monello 7kg $90.000 sin IVA. \n Presiona x para salir.");
     while (opcion != "x" && opcion != "X") {
@@ -51,5 +51,21 @@ if (ingresar) {
 
     }
 
+}*/
+
+const alimentoParaGatos= [{ id:1, nombre: "Monello", peso: "10 kilos", valor: 130.000}, {id:2, nombre: "Hills", peso: "10 kilos", valor: 100.000 }, 
+{id:3, nombre: "Oh my cat", peso: "10 kilos", valor: 70.000}, {id:4, nombre: "Cat Chow", peso: "10 kilos", valor: 40.000}]
+
+console.log(alimentoParaGatos);
+alimentoParaGatos.push({id:5, nombre:"Gatsy", peso: "10 kilos",  valor: 35.000})
+console.log(alimentoParaGatos)
+
+for(const alimento of  alimentoParaGatos){
+    alert("El alimento se llama " +alimento.nombre+ " y tiene un valor de  " +alimento.valor);
+    alimento.id=alimento.id+1;
+    console.log(alimento.id);
+
+    console.log(alimentoParaGatos.indexOf("felix"))
 }
+
 
