84255732ee3460d7a218e3522b09990b866a9e7c
Melissa <mvmelissas@gmail.com>
2022-08-09 17:03:20 -0500

    librerias

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 /*Bienvenida al sitio*/
 
 const btnSaludoBienvenido = document.getElementById("bienvenido");
-const btnSaludoUnico = document.getElementById("unico");
+//const btnSaludoUnico = document.getElementById("unico");
 const titulo = document.querySelector(".entrada")
 
 function saludoBienvenido() {
     alert("Bienvenido/a a CeVeCatdog");
 }
 
-function saludoUnico(nombre) {
+/*function saludoUnico(nombre) {
     alert("Bienvenido/a,  " + nombre)
-}
+}*/
 
 btnSaludoBienvenido.addEventListener("click", saludoBienvenido);
 
@@ -20,10 +20,22 @@ btnSaludoBienvenido.addEventListener("click", saludoBienvenido);
 // SaludoUnico(nombre);
 //})
 
-btnSaludoUnico.onclick = () => {
-    alert("Cuidamos a tu mascota con mucho amor");
-}
+/*btnSaludoUnico.onclick = () => {
+  alert("Cuidamos a tu mascota con mucho amor");
+  }*/
 
+const btnSaludoUnico = document.getElementById("unico");
+btnSaludoUnico.addEventListener("click", ()=>{
+    swal.fire({
+        title: "Saludo",
+        text: "Cuidamos a tu mascota con mucho amor",
+        icon: "info",
+        iconColor: "#00ff00",
+        cnfirmButtonText: "CeVeCatdog",
+        position: "center-start"
+
+    })
+})
 
 /*alimento*/
 
@@ -66,104 +78,5 @@ let usuarioMemo = JSON.parse(sessionStorage.getItem("usuario"));
 console.log(sessionStorage.getItem("usuario"));
 console.log(usuarioMemo);
 
-
-
-
-
-
-/*digite su clave*/
-
-/*let claveGuardada = "Colombia2022";
-let ingresar = false;
-for (let i = 2; i >= 0; i--) {
-    let claveUsuario = prompt("Ingresa tu clave");
-    if (claveGuardada == claveUsuario) {
-        alert("Ingresaste exitosamente");
-        ingresar = true;
-        break;
-    } else {
-        alert("error, te quedan " + i + " intentos")
-    }
-
-}*/
-
-/*opciones carrito*/
-
-
-/*class alimento {
-
-    constructor(nombre, peso, valor, id) {
-        this.nombre = nombre;
-        this.peso = peso;
-        this.valor = valor;
-        this.id = id;
-
-
-    }
-
-    asignarId(array) {
-        this.id = array.lenght;
-    }
-}
-
-
-
-function alimentos(nombre, peso, valor, tipo, id) {
-    this.nombre = nombre;
-    this.peso = peso;
-    this.valor = valor;
-    this.tipo = tipo;
-    this.id = id;
-
-    this.mostrar = function () {
-        console.log("alimento  " + this.nombre, "valor  " + this.valor, "tipo  " + this.tipo)
-    }
-}
-const alimento1 = new alimentos("Monello", 10, 130.000, "renal", 1);
-alimento1.mostrar();
-
-const alimento2 = new alimentos("Hills", "10 kilos", 100.000, "digestive", 2);
-alimento2.mostrar();
-const alimento3 = new alimentos("Oh my cat", "10 kilos", 70.000, "todas las edades", 3);
-alimento3.mostrar();
-
-const alimento4 = new alimentos("Cat Chow", "10 kilos", 40.000, "control peso", 4)
-alimento4.mostrar()
-
-//const filtrado = alimentos.filter((alimento)alimento.precio>80.000)
-//console.log(encontrado)
-
-if (ingresar) {
-    let alimentos= "felino";
-    let opcion = prompt("Escoge el alimento que desees: \n1-Monello 10kg $130.000 sin IVA. \n2-Hills 10kg $100.000. \n3-oh my cat 10kg $70.0000. n\4- cat chow 10 kilos n\Presiona x para salir.");
-    while (opcion != "x" && opcion != "X") {
-        switch (opcion) {
-            case "1":
-                let opcion = prompt("escoge una opcion: \n1-Ir a pagar. \n salir");
-                while (opcion != "salir" && opcion != "SALIR") {
-                    switch (opcion) {
-                        case "1":
-                            const suma = (a, b) => a + b;
-                            const resta = (a, b) => a - b;
-                            const iva = x => x * 0.19
-                            let precioProducto = 130.000
-                            let descuento = 15.000
-                            let precioTotal = resta(suma(precioProducto, iva(precioProducto)), descuento)
-                            console.log(precioTotal);
-
-                            let Total = ("Son $139.700");
-                            break;
-                        
-
-                    }
-
-
-                }
-
-        }
-
-    }
-
-}*/
 
 
